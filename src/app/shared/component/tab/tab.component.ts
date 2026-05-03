bf7b31ef8247643562bc474b9f2207212a150e9a
VaishnaviRW <wadgaonkar7744@gmail.com>
2026-05-03 14:30:50 +0530

    commit

--- src/app/shared/component/tab/tab.component.ts
+++ src/app/shared/component/tab/tab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tabArr } from '../../modules/app';
 
 @Component({
   selector: 'app-render',
@@ -7,18 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabComponent implements OnInit {
 
-  selektedFremWork :string ='angular'
-  constructor() { 
 
+
+selectedskill :string='html'
+
+
+selectedArr :string ='javascript';
+
+
+TabArr1 : tabArr[] = [
+  {
+    id: 'javascript',
+    name: 'JavaScript',
+    defination: 'JavaScript is a programming language used to create dynamic and interactive web pages and modern applications.'
+  },
+  {
+    id: 'html',
+    name: 'HTML',
+    defination: 'HTML is a markup language used to define the structure of web pages using elements like headings, paragraphs, and links.'
+  },
+  {
+    id: 'css',
+    name: 'CSS',
+    defination: 'CSS is used to style and layout web pages, including colors, fonts, spacing, and responsive design.'
+  },
+  {
+    id: 'angular',
+    name: 'Angular',
+    defination: 'Angular is a TypeScript-based framework used to build scalable, dynamic, and single-page web applications.'
   }
+];
 
-  
+
+
+value=123.080;
+value1=23;
+
+
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-      onTabChange(skill :string){
-        this.selektedFremWork=skill
-      }
+onTabChange(skill1 :string){
+  this.selectedArr=skill1
+}
+
+  onTabChang(skill :string){
+    this.selectedskill=skill
+  }
 
 }
